c9870e9a0972d5c5202c65aeecce1dd39873d47d
bhavik <bhavik@pop-os.localdomain>
2018-06-28 18:51:49 -0500

    updated config

--- linux/setup-configurations.sh
+++ linux/setup-configurations.sh
@@ -6,6 +6,6 @@ ln -sf $(pwd)/configurations/i3config ~/.config/i3/.config
 ln -sf $(pwd)configurations/i3statusConfig ~/.config/i3status/
 ln -sf $(pwd)configurations/compton.conf ~/.config/compton.conf
 ln -sf $(pwd)configurations/.xbindkeysrc ~/
-ln -sf $(pwd).themes ~/.themes
+ln -sf $(pwd)/.themes ~/.themes
 
 xbindkeys
