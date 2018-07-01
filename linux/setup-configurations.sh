63c2bca4e55cf35fd64cd0efed59bc329e9e8dd3
name <email>
2018-06-30 22:04:41 -0500

    updated config

--- linux/setup-configurations.sh
+++ linux/setup-configurations.sh
@@ -3,7 +3,7 @@ mkdir ~/.config/i3status/
 mkdir ~/i3/
 
 ln -sf $(pwd)/configurations/i3config ~/.config/i3/.config
-ln -sf $(pwd)configurations/i3statusConfig ~/.config/i3status/
+ln -sf $(pwd)configurations/i3statusConfig ~/.i3status.conf
 ln -sf $(pwd)configurations/compton.conf ~/.config/compton.conf
 ln -sf $(pwd)configurations/.xbindkeysrc ~/
 ln -sf $(pwd)/.themes ~/.themes
