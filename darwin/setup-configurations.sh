6a7fbb847ddd55ceea15faaa10ff7a29bc6a85ba
bhavik <bp1235811>
2018-07-28 08:54:56 -0500

    added animation removal code

--- darwin/setup-configurations.sh
+++ darwin/setup-configurations.sh
@@ -1,2 +1,17 @@
 #create symbolic link for config files
 ln -sf $(pwd)/configurations/.hushlogin ~/.hushlogin
+
+# Disable Open/Close Window Animations
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# Disable Quick Look Animations
+defaults write -g QLPanelAnimationDuration -float 0
+
+# Disable Window Size Adjustment Animations
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Disable Dock Animations
+defaults write com.apple.dock launchanim -bool false
+
+# Disable the “Get Info” Animation
+defaults write com.apple.finder DisableAllAnimations -bool true
