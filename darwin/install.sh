1728499553c99d8bf9094d3d80d8df476480c870
bhavik <bhavik@gopangea.com>
2018-08-06 11:22:26 -0500

    added separate install file

---
+++ darwin/install.sh
@@ -0,0 +1,37 @@
+# Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install apps using cask
+brew cask install zsh
+brew cask install alfred
+brew cask install android-studio 
+brew cask install appcleaner
+brew cask install brave
+brew cask install calibre
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install macdown
+brew cask install mongodb
+brew cask install mongodb-compass
+brew cask install postman
+brew cask install sequel-pro
+brew cask install simplenote
+brew cask install skitch
+brew cask install slack
+brew cask install sourcetree
+brew cask install spotify
+brew cask install teleport
+brew cask install transmission
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install gimp
+
+
+# Giphy capture
+# wunderlist
+# trello
+# BetterSnapTool
+# Amphetamine
