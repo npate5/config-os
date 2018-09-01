4f2aba602d027ee54e708c37a544c32084c37f9f
bhavik <bp1235811@gmail.com>
2018-09-01 17:26:34 -0400

    updated config

--- linux-preinstall.sh
+++ linux-preinstall.sh
@@ -1,5 +1,13 @@
 #GENERATE SSH KEY and ADD IT TO GITHUB
+ssh-keygen -t rsa -C "bp1235811@gmail.com" -f /home/bhavik/.ssh/id_rsa_personal -q -N ""
+echo "\nAdd this key to personal Github Account:\n"
+cat ~/.ssh/id_rsa_personal.pub
+echo "\n\n"
 
+ssh-keygen -t rsa -C "bhavik@work.com" -f /home/bhavik/.ssh/id_rsa_work -q -N ""
+echo "\nAdd this key to work Github Account:\n"
+cat ~/.ssh/id_rsa_work.pub
+echo "\n\n"
 
 #Download configos rep
 cd
