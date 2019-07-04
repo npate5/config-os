bf45ca3c318b8c1e7d13d7da1ecae3d37d336a0b
bhavik <bhavik@email.com>
2019-07-03 19:23:43 -0500

    updated config

--- linux-install.sh
+++ linux-install.sh
@@ -16,7 +16,10 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Install 
-sudo apt-get install -y unzip htop curl wget gpick build-essential cmake git gimp zsh nodejs npm flameshot calibre gnome-tweak-tool gnome-disk-utility tree qemu-kvm papirus-icon-theme xbindkeys xautomation exfat-fuse exfat-utils xpad timeshift gparted gnome-shell-extensions chrome-gnome-shell 
+sudo apt-get install -y unzip htop curl wget gpick build-essential cmake git gimp zsh flameshot calibre gnome-disk-utility tree qemu-kvm papirus-icon-theme xbindkeys xautomation exfat-fuse exfat-utils xpad timeshift gparted 
+#Gnome Specific
+sudo apt-get install gnome-shell-extensions chrome-gnome-shell gnome-tweak-tool pdftk
+
 
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
--- linux-startup.sh
+++ linux-startup.sh
@@ -1,5 +1,5 @@
 xinput --set-prop "Logitech M570" "libinput Accel Speed" -0.9
-setxkbmap -option caps:escape
-xgamma -gamma .9
-compton
+# setxkbmap -option caps:escape
+# xgamma -gamma .9
+# compton
 #xmodmap ~/.Xmodmap #add this to startup manually
