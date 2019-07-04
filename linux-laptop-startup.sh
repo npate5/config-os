de1a8bc5232cde9461ff71a37c556dfe2189b1c8
bhavik <bhavik@email.com>
2019-07-03 19:27:02 -0500

    updated config

--- linux-laptop-startup.sh
+++ linux-laptop-startup.sh
@@ -1,4 +1,4 @@
-xinput --set-prop "Logitech M570" "libinput Accel Speed" -0.9 
+gxinput --set-prop "Logitech M570" "libinput Accel Speed" -0.9 
 #/usr/bin/setxkbmap -option "ctrl:nocaps"
 #/usr/bin/setxkbmap -option altwin:swap_alt_win
 xgamma -gamma .9
