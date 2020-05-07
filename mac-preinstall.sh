240f2373e37779af7e64dc93f2a6f768950694c4
npate5 <nehapatel8306@gmail.com>
2020-05-06 19:11:12 -0500

    made changes to install script

--- mac-preinstall.sh
+++ mac-preinstall.sh
@@ -6,18 +6,12 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #GENERATE SSH KEY and ADD IT TO GITHUB
-ssh-keygen -t rsa -C "persoanlAddress@emailhost.com" -f /Users/bhavik/.ssh/id_rsa_personal -q -N ""
+ssh-keygen -t rsa -C "nehapatel8306@gmail.com" -f /Users/neha/.ssh/id_rsa_personal -q -N ""
 echo "\nAdd this key to personal Github Account:\n"
 cat ~/.ssh/id_rsa_personal.pub
 echo "\n\n"
 
-ssh-keygen -t rsa -C "workAddress@emailhost.com" -f /Users/bhavik/.ssh/id_rsa_work -q -N ""
-echo "\nAdd this key to work Github Account:\n"
-cat ~/.ssh/id_rsa_work.pub
-echo "\n\n"
-
 ssh-add ~/.ssh/id_rsa_personal
-ssh-add ~/.ssh/id_rsa_work
 
-git config --global user.email "bhavik@email.com"
-git config --global user.name "bhavik"
+git config --global user.email "nehapatel8306@gmail.com"
+git config --global user.name "neha"
